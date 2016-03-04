9e5c9926a4ce63edf0f29b10a07922b579ceb5cd
nice295 <nice295@gmail.com>
2016-03-05 01:45:13 +0900

    .

---
+++ 04.Pay/pay.framer/app.coffee
@@ -0,0 +1,61 @@
+# Device
+Framer.Device.deviceType = "samsung-galaxy-note-5-gold"
+
+# Import file "Pay"
+sketch = Framer.Importer.load("imported/Pay@1x")
+
+sketch.simple.draggable.enabled = true
+sketch.simple.draggable.horizontal = false
+
+sketch.simple.states.add
+	panel:
+		y: 2560 - 180 / 2
+	down:
+        y: 2560 - 180   
+    up:
+        y: 548
+
+sketch.main.states.add
+    down:
+        y: 2560
+    up:
+        y: 0
+
+# Initial
+sketch.home.visible = true
+sketch.simple.visible = true
+sketch.main.visible = true
+sketch.simple.y = 2560 - 180 / 2
+sketch.main.y = 2560
+sketch.main.states.switch("down")
+sketch.simple.states.switch("panel")
+                
+# Animation
+        
+# Events
+sketch.simple.on Events.DragEnd, (event, draggable, layer) ->
+	if sketch.simple.draggable.offset.y < -400 #up
+		sketch.simple.states.switch("up", time: 0.4, curve: "ease-in-out")
+		if sketch.main.states.current == "down"
+			sketch.main.states.switch("up", time: 0.4, curve: "ease-in-out")
+	else if sketch.simple.draggable.offset.y > 400 #down
+		#print "down"
+		sketch.simple.states.switch("down", time: 0.4, curve: "ease-in-out")
+	else
+		#print "nothing"
+		sketch.simple.states.switch(sketch.simple.states.current, time: 0.4, curve: "ease-in-out")
+    
+sketch.home_icon.onClick ->
+	sketch.simple.states.switch("down", delay: 0.4, time: 0.4, curve: "ease-in-out")
+	sketch.main.states.switch("up", time: 0.4, curve: "ease-in-out")
+
+sketch.simple_handle.onClick ->
+	sketch.simple.states.switch("down", time: 0.4, curve: "ease-in-out")
+	sketch.main.states.switch("down", delay: 0.4, time: 0.4, curve: "ease-in-out")
+
+sketch.home_handle.onClick ->
+	sketch.simple.visible = true
+	sketch.simple.states.switch("up", delay: 0.4, time: 0.4, curve: "ease-in-out")
+	sketch.main.visible = true
+	sketch.main.states.switch("up", time: 0.4, curve: "ease-in-out")
+	
---
+++ 04.Pay/pay.framer/imported/Pay@1x/layers.json.js
@@ -0,0 +1,198 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["Pay/layers.json.js"] = [
+  {
+    "maskFrame" : null,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "9E883FD4-2DB6-45A2-AC85-099E9E3D33D9",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-simple_handle-9E883FD4-2DB6-45A2-AC85-099E9E3D33D9.png",
+              "frame" : {
+                "y" : 548,
+                "x" : 0,
+                "width" : 1440,
+                "height" : 180
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 548,
+              "x" : 0,
+              "width" : 1440,
+              "height" : 180
+            },
+            "name" : "simple_handle"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "93EFCE5D-402B-4B7F-934B-135C9600ECE2",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-simple_bg-93EFCE5D-402B-4B7F-934B-135C9600ECE2.png",
+              "frame" : {
+                "y" : 548,
+                "x" : 0,
+                "width" : 1440,
+                "height" : 2000
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 548,
+              "x" : 0,
+              "width" : 1440,
+              "height" : 2000
+            },
+            "name" : "simple_bg"
+          }
+        ],
+        "id" : "F15CB80C-1675-4039-929E-579FE08C5526",
+        "visible" : true,
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 548,
+          "x" : 0,
+          "width" : 1440,
+          "height" : 2000
+        },
+        "name" : "simple"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "B1FB4E29-1946-441D-9FD5-1007921CDAB9",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-main-B1FB4E29-1946-441D-9FD5-1007921CDAB9.png",
+          "frame" : {
+            "y" : 0,
+            "x" : 0,
+            "width" : 1440,
+            "height" : 2560
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 0,
+          "x" : 0,
+          "width" : 1440,
+          "height" : 2560
+        },
+        "name" : "main"
+      },
+      {
+        "maskFrame" : null,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "D1FA3E0E-922F-481D-B598-FB1A3DFD69C9",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-home_handle-D1FA3E0E-922F-481D-B598-FB1A3DFD69C9.png",
+              "frame" : {
+                "y" : 2470,
+                "x" : 309,
+                "width" : 823,
+                "height" : 90
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 2470,
+              "x" : 309,
+              "width" : 823,
+              "height" : 90
+            },
+            "name" : "home_handle"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "3AD39181-D41D-4A36-B0FE-7EC1E868B7A1",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-home_icon-3AD39181-D41D-4A36-B0FE-7EC1E868B7A1.png",
+              "frame" : {
+                "y" : 1568,
+                "x" : 192,
+                "width" : 176,
+                "height" : 172
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 1568,
+              "x" : 192,
+              "width" : 176,
+              "height" : 172
+            },
+            "name" : "home_icon"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "8A5C9A17-4D4D-42BF-8CA3-C512447E5CED",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-home_bg-8A5C9A17-4D4D-42BF-8CA3-C512447E5CED.png",
+              "frame" : {
+                "y" : 0,
+                "x" : 0,
+                "width" : 1440,
+                "height" : 2560
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 0,
+              "x" : 0,
+              "width" : 1440,
+              "height" : 2560
+            },
+            "name" : "home_bg"
+          }
+        ],
+        "id" : "72619B58-5F78-49F2-803D-1B921CB15672",
+        "visible" : true,
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 0,
+          "x" : 0,
+          "width" : 1440,
+          "height" : 2560
+        },
+        "name" : "home"
+      }
+    ],
+    "id" : "0E2E24E1-CA0C-4CFB-A42C-93EBB1FB7AB4",
+    "visible" : true,
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 1440,
+      "height" : 2560
+    },
+    "name" : "Mobile_Portrait"
+  }
+]
